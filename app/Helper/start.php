ec0d40aee41211bab8465ad443ac27ce842830fb
Marxio Badel <marxiobadel@gmail.com>
2026-01-23 02:42:12 +0100

    Project initialized

---
+++ app/Helper/start.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Support\Number;
+use Illuminate\Support\Str;
+
+if (! function_exists('generateUniqueRef')) {
+    function generateUniqueRef($modelClass, $column = 'reference', int $length = 8, ?string $prefix = null)
+    {
+        do {
+            $reference = ($prefix ?? '').Str::upper(Str::random($length));
+        } while ($modelClass::where($column, $reference)->exists());
+
+        return $reference;
+    }
+}
+
+if (! function_exists('toCurrency')) {
+    function toCurrency(int|float $amount)
+    {
+        return Number::currency($amount);
+    }
+}
+
+if (! function_exists('toPercentage')) {
+    function toPercentage(int|float $number, $maxPrecision = 2)
+    {
+        return Number::percentage($number, maxPrecision: $maxPrecision);
+    }
+}
+
+if (! function_exists('toAbbreviate')) {
+    function toAbbreviate(int|float $number, $maxPrecision = 2)
+    {
+        return Number::abbreviate($number, maxPrecision: $maxPrecision);
+    }
+}
+
+if (! function_exists('toFormat')) {
+    function toFormat(int|float $number, $maxPrecision = 2)
+    {
+        return Number::format($number, maxPrecision: $maxPrecision);
+    }
+}
+
+if (! function_exists('plural')) {
+    function plural(string $value, int $count = 2)
+    {
+        return Str::plural($value, max($count, 1));
+    }
+}
+
+if (! function_exists('generateHexColor')) {
+    function generateHexColor()
+    {
+        return '#'.substr(str_shuffle('ABCDEF0123456789'), 0, 6);
+    }
+}
+
+if (! function_exists('generateMultipleHexColors')) {
+    function generateMultipleHexColors($count = 1)
+    {
+        $colors = [];
+
+        for ($i = 0; $i < $count; $i++) {
+            $colors[] = generateHexColor();
+        }
+
+        return $colors;
+    }
+}
+
+if (! function_exists('urlExists')) {
+    /**
+     * Check if a URL returns a valid response (200).
+     */
+    function urlExists(string $url): bool
+    {
+        $headers = @get_headers($url);
+
+        return $headers && strpos($headers[0], '200') !== false;
+    }
+}
